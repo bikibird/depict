8e277e1720ea1f4aa4c9c14445a82455c951213c
bikibird <jenny@bikibird.com>
2021-02-07 17:00:57 -0600

    Updated

--- depict.js
+++ depict.js
@@ -23,16 +23,18 @@ SOFTWARE.
 */
 var RGB=class RGB
 {
-	constructor(red,green,blue)
+	constructor(red,green,blue,picoIndex)
 	{
 		Object.defineProperty(this,"r",{writable: true})
 		Object.defineProperty(this,"g",{writable: true})
 		Object.defineProperty(this,"b",{writable: true})
+		Object.defineProperty(this,"picoIndex",{value:picoIndex,writable: true})
 		if (red instanceof RGB)
 		{	
 			this.r=red.r
 			this.g=red.g
 			this.b=red.b
+			this.picoIndex=red.picoIndex
 		}
 		else
 		{
@@ -69,29 +71,22 @@ for (let i=0;i<256;i++)
 }
 var Colorspace=class Colorspace
 {
-	constructor(palette=[],shades=17)
+	constructor(palette=[],shades)
 	{
+		//palette=palette.map(entry=>new RGB(entry))
 		Object.defineProperty(this,"colors",{value:[],writable: true})
 		Object.defineProperty(this,"shades",{value:shades,writable: true})
-		Object.defineProperty(this,"palette",{value:palette.map(entry=>new RGB(entry)),writable: true})
 		for (let i = 0; i < palette.length-1; i++)
 		{
 			for (let j = i+1; j < palette.length; j++)
 			{
 				this.add(palette[i],palette[j],shades)
 			}
+
 		}
 	}
 	add(rgb1, rgb2, shades)
 	{
-		if (!(rgb1 instanceof RGB))
-		{
-			rgb1=new RGB(rgb1)
-		}
-		if (!(rgb2 instanceof RGB))
-		{
-			rgb2=new RGB(rgb2)
-		}
 		var divisor=shades-1
 		for (let i= 0; i < divisor; i++)
 		{
@@ -114,107 +109,6 @@ var Colorspace=class Colorspace
 		}
 		return this.colors[nearestIndex]
 	}
-	nearestPaletteEntry(rgb)
-	{
-		var nearestIndex=0
-		var nearestDistance=rgb.distance(this.palette[nearestIndex])
-		for (let i=1; i<this.palette.length; i++)
-		{
-			var distance=rgb.distance(this.palette[i])
-			if (distance<nearestDistance)
-			{
-				nearestIndex=i
-				nearestDistance=distance
-			}
-		}
-		return this.palette[nearestIndex]
-	}
-	quantize(...args)
-	{
-		var {context,cap}=args[0]
-		var {data:pixels,width, height}=context.getImageData(0, 0, context.canvas.width, context.canvas.height)
-		var paintPots=[]
-		for(let j=0;j<cap;j++)
-		{
-			var i=Math.floor(Math.random()*width*4*height+Math.random()*width*4)
-			paintPots[j]={rgb:new RGB(pixels[i],pixels[i+1],pixels[i+2]),drops:1}
-		}
-		function mix(paintPots)
-		{
-			var paints=[]
-			for (let j=0;j<paintPots.length;j++)
-			{
-				paints[j]={rgb:new RGB(paintPots[j].rgb),drops:1}
-			}
-			for(let y=0;y<height;y++)
-			{
-				for(let x=0;x<width;x++)
-				{
-					var i=width*4*y+x*4
-					var nearestDistance=Infinity
-					var nearestIndex=0
-					var distance
-					var pixel=new RGB(pixels[i],pixels[i+1],pixels[i+2])
-					for (let j=0;j<paints.length;j++)
-					{
-						distance=paints[j].rgb.distance(pixel)
-						if (distance<nearestDistance)
-						{
-							nearestIndex=j
-							nearestDistance=distance
-						}
-					}
-					var drops=paints[nearestIndex].drops
-					var divisor=drops+1
-					var paint=paints[nearestIndex].rgb
-					paint.r=Math.floor(((paint.r*drops+pixel.r)/divisor)+.5)
-					paint.g=Math.floor(((paint.g*drops+pixel.g)/divisor)+.5)
-					paint.b=Math.floor(((paint.b*drops+pixel.b)/divisor)+.5)
-					paints[nearestIndex].drops++
-				}	
-			}
-			return paints
-		}
-		var done=false
-		var tempPaintPots
-		while(!done)
-		{
-			tempPaintPots=mix(paintPots)
-			done=true
-			for (let j=0;j<paintPots.length;j++)
-			{
-				if(paintPots[j].rgb.r!=tempPaintPots[j].rgb.r || paintPots[j].rgb.g!=tempPaintPots[j].rgb.g || paintPots[j].rgb.b!=tempPaintPots[j].rgb.b)
-				{
-					done=false
-					paintPots=tempPaintPots
-				}
-			}
-		}
-		//match paintpots to nearests colors in colorspace
-		var colors=[]
-		for (let j=0;j<paintPots.length;j++)
-		{
-			var color=this.nearestPaletteEntry(paintPots[j].rgb)
-			colors.push(color)
-			/*if (color.blend===0)
-			{
-				colors.push(color.rgb1)
-			}
-			else
-			{
-				if (color.blend===1)
-				{
-					colors.push(color.rgb2)
-				}
-				else
-				{
-					colors.push(color.rgb1)
-					colors.push(color.rgb2)
-				}
-			} */
-		}
-		return new Colorspace(colors,this.shades)
-	}
 }
 var Color=class Color
 {
@@ -236,14 +130,8 @@ var Picture =  class Picture
 	constructor(context, width, height)
 	{
 		Object.defineProperty(this,"pixels",{value:context.getImageData(0, 0, width, height),writable: true})		
-		Object.defineProperty(this,"original",{value:this.pixels,writable: true})
 		Object.defineProperty(this,"width",{value:width,writable: true})				
 		Object.defineProperty(this,"height",{value:height,writable: true})
-	}
-	revert()
-	{
-		this.pixels=this.original
-		return this
 	}
 	rgb(x,y)
 	{
@@ -258,29 +146,12 @@ var Picture =  class Picture
 		this.pixels.data[i+2]=rgb.b
 		return this
 	}
-
-	crop(x,y,width,height)
-	{
-
-	}
-	mirror()
-	{
-
-	}
-	flip()
-	{
-
-	}
-	resize()
-	{
-
-	}
 }
-var depict =function depict({original,palette,filter,cap}={})
+var depict =function depict({original,palette,filter}={})
 {
 	var depiction=document.createElement("canvas")
 	var preview=document.createElement("canvas")
-	
+	var colorsUsed=new Set()
 	var aspect=original.width/original.height
 	if (original.width>original.height)
 	{
@@ -312,28 +183,22 @@ var depict =function depict({original,palette,filter,cap}={})
 	depiction.height = height
 	depictionContext = depiction.getContext("2d")
 	depictionContext.drawImage(original, 0, 0, width, height)
-
-	var colorspace=new Colorspace(palette,filter.shades)
-/*	for (let i = 0; i < palette.length-1; i++)
-	{
-		for (let j = i+1; j < palette.length; j++)
-		{
-			colorspace.add(palette[i],palette[j],filter.shades)
-		}
-	}
-*/	
-	if (palette.length>cap){colorspace=colorspace.quantize({context:depictionContext,cap:cap})}
-
+	var size=filter.matrix.length
+	var colorspace=new Colorspace(palette,size**2+1)
 	var ditherRGB
 	var picture = new Picture(depictionContext, width, height)
-
+	var pixel, nearestColor,threshold
+	var matrix=filter.matrix
+	var redError, greenError, blueError
+	var ex,ey
 	for (let y = 0; y < picture.height; y++)
 	{
 		for (let x = 0; x <picture.width; x++)
 		{
-			var pixel=picture.rgb(x,y)
-			var nearestColor=colorspace.nearest(pixel)
-			var threshold=filter.grid[x%filter.size][y%filter.size]
+			pixel=picture.rgb(x,y)
+			nearestColor=colorspace.nearest(pixel)
+			
+			threshold=matrix[x%size][y%size]
 			if (nearestColor.blend>=threshold)
 			{
 				picture.set(x,y,nearestColor.rgb2)
@@ -344,10 +209,10 @@ var depict =function depict({original,palette,filter,cap}={})
 				picture.set(x,y,nearestColor.rgb1)
 				ditherRGB=nearestColor.rgb1
 			}
-			var redError=pixel.r-ditherRGB.r
-			var greenError=pixel.g-ditherRGB.g
-			var blueError=pixel.b-ditherRGB.b
-			var ex,ey
+			redError=pixel.r-ditherRGB.r
+			greenError=pixel.g-ditherRGB.g
+			blueError=pixel.b-ditherRGB.b
+			colorsUsed.add(ditherRGB.picoIndex)
 			filter.corrections.forEach(point=>
 			{
 				ex=x+point.x
@@ -356,9 +221,9 @@ var depict =function depict({original,palette,filter,cap}={})
 				{
 					eRGB=picture.rgb(ex,ey)
 
-					eRGB.r=Math.min(Math.max(eRGB.r +Math.floor(redError * point.amount +.5), 0), 255)
-					eRGB.g=Math.min(Math.max(eRGB.g + Math.floor(greenError * point.amount +.5), 0), 255)
-					eRGB.b=Math.min(Math.max(eRGB.b + Math.floor(blueError * point.amount +.5), 0), 255)
+					eRGB.r=Math.min(Math.max(eRGB.r +Math.floor(redError * point.amount+.5), 0), 255)
+					eRGB.g=Math.min(Math.max(eRGB.g + Math.floor(greenError * point.amount+.5), 0), 255)
+					eRGB.b=Math.min(Math.max(eRGB.b + Math.floor(blueError * point.amount+.5), 0), 255)
 					picture.set(ex,ey,eRGB)
 				}	
 			})
@@ -383,23 +248,18 @@ var depict =function depict({original,palette,filter,cap}={})
 			previewData.data[i+2]=picture.pixels.data[j+2]
 			previewData.data[i+3]=picture.pixels.data[j+3]
 		}
-		
 	}
 	previewContext.putImageData(previewData, 0, 0)
-	return {depiction:depiction,preview:preview}
+	return {depiction:depiction,preview:preview, colorsUsed:[...colorsUsed].sort((a,b)=>a-b)}
 }
 var filters={}
 filters["Solid"]={
-	shades:2,
-	size:1,
-	grid:[[1/2]],
+	matrix:[[1/2]],
 	corrections:[],
 	default:false
 }
 filters["5 Shade Dither"]={
-	shades:5,
-	size:2,
-	grid:[
+	matrix:[
 		[1/5,3/5],
 		[4/5,2/5]
 	],
@@ -407,9 +267,7 @@ filters["5 Shade Dither"]={
 	default:false
 }
 filters["10 Shade Dither"]={
-	shades:10,
-	size:3,
-	grid:[
+	matrix:[
 		[3/10,7/10,4/10],
 		[6/10,1/10,9/10],
 		[2/10,8/10,5/10]
@@ -418,9 +276,7 @@ filters["10 Shade Dither"]={
 	default:false
 }
 filters["17 Shade Dither"]={
-	shades:17,
-	size:4,
-	grid:[
+	matrix:[
 		[1/17,9/17,3/17,11/17],
 		[13/17,5/17,15/17,7/17],
 		[4/17,12/17,2/17,10/17],
@@ -430,9 +286,7 @@ filters["17 Shade Dither"]={
 	default:true
 }
 filters["Atkinson"]={
-	shades:2,
-	size:1,
-	grid:[[1/2]],
+	matrix:[[1/2]],
 	corrections:[
 		{x:1,y:0,amount:1/8},
 		{x:2,y:0,amount:1/8},
@@ -444,9 +298,7 @@ filters["Atkinson"]={
 	default:false
 }
 filters["Floyd-Steinberg"]={
-	shades:2,
-	size:1,
-	grid:[[1/2]],
+	matrix:[[1/2]],
 	corrections:[
 		{x:1,y:0,amount:7/16},
 		{x:-1,y:1,amount:3/16},
@@ -456,9 +308,7 @@ filters["Floyd-Steinberg"]={
 	default:false
 }
 filters["Sierra2"]={
-	shades:2,
-	size:1,
-	grid:[[1/2]],
+	matrix:[[1/2]],
 	corrections:[
 		{x:1,y:0,amount:4/16},
 		{x:2,y:0,amount:3/16},
@@ -470,4 +320,45 @@ filters["Sierra2"]={
 	],
 	default:false
 }
-
+filters["Rivers"]={
+	matrix:[[1/2]],
+	corrections:[
+		{x:-1,y:1,amount:1/3},
+		{x:0,y:1,amount:1/3},
+		{x:1,y:1,amount:1/3}
+	],
+	default:false
+}
+filters["Streets"]={
+	matrix:[[1/2]],
+	corrections:[
+		{x:1,y:0,amount:1/4},
+		{x:-1,y:1,amount:1/4},
+		{x:0,y:1,amount:1/4},
+		{x:1,y:1,amount:1/4}
+	],
+	default:false
+}
+filters["Rain"]={
+	matrix:[[1/2]],
+	corrections:[
+		{x:1,y:0,amount:1/2},
+		{x:1,y:1,amount:1/2},
+	],
+	default:false
+}
+filters["Wind"]={
+	matrix:[[1/2]],
+	corrections:[
+		{x:1,y:0,amount:2/12},
+		{x:2,y:0,amount:1/12},
+		{x:0,y:1,amount:2/12},
+		{x:1,y:1,amount:2/12},
+		{x:2,y:1,amount:1/12},
+		{x:-1,y:2,amount:1/12},
+		{x:0,y:2,amount:1/12},
+		{x:1,y:2,amount:1/12},
+		{x:2,y:2,amount:1/12}
+	],
+	default:false
+}
